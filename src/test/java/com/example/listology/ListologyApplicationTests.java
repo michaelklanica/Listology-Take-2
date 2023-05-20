a09e9d6251b6eff22df4eec2d5127cb93b372abd
mklanica <michael.david.klanica@gmail.com>
2023-05-20 00:15:36 -0500

    Initial commit

---
+++ src/test/java/com/example/listology/ListologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
