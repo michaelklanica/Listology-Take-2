c38b45ab31d99f995c6ceee2152647cb5955a398
mklanica <michael.david.klanica@gmail.com>
2023-05-25 23:42:46 -0500

    rethinking the structure of the UI, deleted many files and left backend code remaining.

--- src/main/java/com/example/listology/controller/UserController.java
+++ src/main/java/com/example/listology/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.listology.controller;
 
-import com.example.listology.dto.UserDTO;
+import com.example.listology.model.User;
 import com.example.listology.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-// Import necessary libraries and annotations
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -18,20 +21,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
-        // Validate user input (e.g., username uniqueness, password requirements, etc.)
 
-        // Create a new user based on the userDTO
-        UserDTO newUser = new UserDTO();
-        newUser.setUsername(userDTO.getUsername());
-        newUser.setEmail(userDTO.getEmail());
-        newUser.setPassword(userDTO.getPassword());
 
-        // Save the new user to the database using the userService
-        userService.registerUser(newUser);
-
-        // Return a success message
-        return ResponseEntity.ok("User registered successfully");
-    }
+    // Other methods and functionalities for user-related operations
 }
--- src/main/java/com/example/listology/service/UserService.java
+++ src/main/java/com/example/listology/service/UserService.java
@@ -1,6 +1,5 @@
 package com.example.listology.service;
 
-import com.example.listology.dto.UserDTO;
 import com.example.listology.model.User;
 import com.example.listology.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,24 +15,11 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void registerUser(UserDTO userDTO) {
-        // Check if the username or email already exists in the database
-        if (userRepository.existsByUsername(userDTO.getUsername())) {
-            throw new RuntimeException("Username already exists");
-        }
+    public User createUser(User user) {
+        // Perform any necessary validation or checks on the user data
 
-        if (userRepository.existsByEmail(userDTO.getEmail())) {
-            throw new RuntimeException("Email already exists");
-        }
-
-        // Create a new User entity
-        User user = new User();
-        user.setUsername(userDTO.getUsername());
-        user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
-
-        // Save the user to the database
-        userRepository.save(user);
+        // Save the user using the UserRepository
+        return userRepository.save(user);
     }
 
     // Other user-related methods
