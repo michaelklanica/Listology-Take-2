94512f104a6dc4990b04d77aa5b8b0934afabead
mklanica <michael.david.klanica@gmail.com>
2023-05-26 23:45:33 -0500

    models rebuilt, user can register for the website and login.  security has not yet been implemented.

--- src/main/java/com/example/listology/controller/LandingController.java
+++ src/main/java/com/example/listology/controller/LandingController.java
@@ -1,13 +1,48 @@
 package com.example.listology.controller;
 
+import com.example.listology.model.User;
+import com.example.listology.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class LandingController {
-
+    @Autowired
+    private UserService userService;
     @GetMapping("/")
     public String showLandingPage() {
         return "landing";
     }
+
+    @GetMapping("/main")
+    public String showMainPage() {
+        // Logic to retrieve data and prepare the main page
+        return "main"; // Return the name of the main page template (e.g., "main.html")
+    }
+
+    @PostMapping("/login")
+    public String processLogin(User user, HttpSession session, Model model) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+
+        // Retrieve the user from the database based on the username
+        User existingUser = userService.getUserByUsername(username);
+
+        // Check if the user exists and the password is correct
+        if (existingUser != null && existingUser.getPassword().equals(password)) {
+            // Authentication successful, store the user object in the session
+            session.setAttribute("loggedInUser", existingUser);
+            return "redirect:/main"; // Redirect to the main page
+        } else {
+            // Authentication failed, display an error message
+            model.addAttribute("error", "Invalid username or password");
+            return "landing";
+        }
+    }
+
 }
---
+++ src/main/java/com/example/listology/service/UserService.java
@@ -0,0 +1,40 @@
+package com.example.listology.service;
+
+import com.example.listology.model.User;
+import com.example.listology.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String createUser(User user) {
+        String username = user.getUsername();
+        String email = user.getEmail();
+
+        // Check if a user with the same username or email already exists
+        if (userRepository.existsByUsername(username)) {
+            return "Username already exists";
+        }
+
+        if (userRepository.existsByEmail(email)) {
+            return "Email address already exists";
+        }
+
+        // Save the user to the database
+        userRepository.save(user);
+
+        return "User created successfully";
+    }
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
